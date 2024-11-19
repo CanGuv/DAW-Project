e572a21c4e206b43a8c16e1d0b1855aee8c8efcd
Babacan Guven <baba1903@Babacans-MacBook-Air.local>
2024-11-19 01:19:35 +0000

    Finished register page with css

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator')
 
 const bcrypt = require('bcrypt')
 const saltRounds = 10
@@ -10,8 +10,37 @@ router.get('/register', function (req, res) {
     res.render('register.ejs')                                                               
 })
 
-router.post('/registered', function (req, res, next) {
+router.post('/registered', [check('email').isEmail(), check('username').isLength({ min: 8 }), check('password').isLength({ min: 8 }).matches(/[A-Z]/).matches(/\d/).matches(/[@$!%*?&]/), check('age').optional().isInt(),  check('height').optional().isFloat(),  check('weight').optional().isFloat()], function (req, res, next) {
+    req.body.username = req.sanitize(req.body.username);
+    req.body.email = req.sanitize(req.body.email);
+    req.body.gender = req.sanitize(req.body.gender)
+    req.body.age = req.sanitize(req.body.age)
+    req.body.height = req.sanitize(req.body.height)
+    req.body.weight = req.sanitize(req.body.weight)
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.redirect('/register'); }
+    else { 
+        const plainPassword = req.body.password
 
+        bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+            if (err) {
+                return next(err); // Handle hashing errors
+            }
+            // Store hashed password in your database.
+            let sqlquery = `INSERT INTO users (username, email, password_hash, age, gender, height, weight) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+            let data = [req.body.username, req.body.email, hashedPassword, req.body.age, req.body.gender, req.body.height, req.body.weight];
+            // Execute the query, passing in the user data
+            db.query(sqlquery, data, (err, results) => {
+                if (err) {
+                    return next(err); // Handle database errors
+                }
+
+                res.send('You are all set up!')
+            });
+        })
+    }
 })
 
 router.get('/login', function (req, res){
